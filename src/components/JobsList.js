2ba5a76e774e0f96046293e4e689249c54a99a1f
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-11-13 19:10:43 -0800

    Map through data.js to create usuable props

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -1,4 +1,11 @@
 import React from "react";
+import data from "../data";
+import Job from "./Job";
 
 export default function JobsList() {
+  const jobs = data.map((item) => {
+    return <Job key={item.id} item={item} />;
+  });
+
+  return <div className="jobs-list">{jobs}</div>;
 }
