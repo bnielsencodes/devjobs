2b51b702b73268ec4fb474db0f6fd1c7823116f8
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-11-13 19:11:32 -0800

    Create MoreButton component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import JobsList from "./JobsList";
+import MoreButton from "./MoreButton";
 
 export default function Main() {
   return (
     <section className="main">
       <SearchBar />
       <JobsList />
+      <MoreButton />
     </section>
   );
 }
