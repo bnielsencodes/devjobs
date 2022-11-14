b2cb7440f910f5e87153f81325dcf1f074b047f9
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-11-13 19:09:52 -0800

    Create JobsList component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import JobsList from "./JobsList";
 
 export default function Main() {
   return (
     <section className="main">
       <SearchBar />
+      <JobsList />
     </section>
   );
 }
